55be9967250494a86f2b01719a795f69a515ab6e
reactVishnu <thirdparties47@gmail.com>
2023-08-02 22:34:27 +0530

    First Change

---
+++ Big O/Big_O(n)_exercise.py
@@ -0,0 +1,37 @@
+# Implement a Python function called print_items.
+
+# This function should take a single integer as an argument and print out a sequence of numbers from 0 up to, but not including, the provided integer.
+
+# The function should use a for loop and Python's built-in range function to generate the sequence of numbers.
+
+# The function signature should be: def print_items(n):
+
+
+# Example:
+
+# Here is an example of how your function should behave:
+
+
+# If you call print_items(10), your function should print:
+
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
+def print_itmes(n):
+    for i in range(n):
+        print(i)
+print_itmes
+
+
+list1 = {1,2,3,4}
+list2 = list1
+list2.add(5)
+print(f'List 1 is {list1}, and list 2 is {list2}')
---
+++ Big O/README.md
@@ -0,0 +1,20 @@
+# Big O - 
+>way of **comparing two piece of code** in a way that we could know how efficient a code was
+
+**Time Complexity:** - about time
+**Space Complexity :** - about memory space
+
+>**Omaega**(best case scenario) , **Theta**(avg case scenario) , **Omegron**(worst case scenario)
+
+>Big O(n):  Eg: - 
+def print_itmes(n):
+    for i in range(n):
+        print(i)
+print_itmes(10)
+it gonna return 0...9 
+it is the example of O(n) cuz we pass the no (n), and it ran n times or n operations,
+
+On a graph
+it always gonnna be a straight line or a propotional line,
+
+![image]https://miro.medium.com/v2/resize:fit:1358/1*yiyfZodqXNwMouC0-B0Wlg.png
